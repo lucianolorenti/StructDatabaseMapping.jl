ef049805ace08ede020365d2f26f637af8e3d7e8
Luciano Rolando Lorenti <lucianolorenti@gmail.com>
2020-02-25 16:48:30 +0100

    Add .gitignore

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,6 @@ using Documenter, StructDatabaseMapping
 
 makedocs(sitename="Struct Database Mapping",
          pages = ["example.md"])
+deploydocs(
+            repo = "github.com/lucianolorenti/StructDatabaseMapping.jl.gitt",
+        )
