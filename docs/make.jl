edc790b51ac0c64b29f4fdf820f525fde7260751
Luciano Rolando Lorenti <lucianolorenti@gmail.com>
2020-03-04 16:57:15 +0100

    Improve documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,12 @@
 using Documenter, StructDatabaseMapping
 
 makedocs(sitename="Struct Database Mapping",
-         pages = ["example.md"])
+        format = Documenter.HTML(prettyurls = false),
+         pages = [ 
+            "Home" => "index.md",
+            "Api" => "api.md",
+            "Example"=>"example.md"]
+        )
 
 deploydocs(
     repo="github.com/lucianolorenti/StructDatabaseMapping.jl.git")
